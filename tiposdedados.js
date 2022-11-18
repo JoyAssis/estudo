69196780c88046d330d09b8d375c002f9c06ac36
Instituto Precisa Ser <joyceassis96@gmail.com>
2022-11-18 18:28:08 -0300

    tipos de dados

---
+++ tiposdedados.js
@@ -0,0 +1,19 @@
+/* Tipos de Dados
+String - cadeia de caracteres usado para criar textos
+"" aspas duplas '' aspas simples `` templates literais ou templte strings
+
+*/
+console.log("Bem-vinda Joy")
+
+/* Number - números
+33// inteiros
+12.5//reais float
+NaN// not a number
+Infinity // infinito
+
+*/
+console.log (12.5)
+
+/* Boolean - números
+ true or false - verdadeiro ou falso
+*/
